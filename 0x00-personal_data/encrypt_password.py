3e6d9eb14b8c1ede9ba8d64b36cffb205ed4cfb2
betiniakarandut <betiniakarandut@gmail.com>
2023-09-01 03:30:19 +0100

    Hashed password with bcrypt

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,16 @@
+# !/usr/bin/env python3
+"""Module for bcrypt"""
+import bcrypt
+
+
+def hash_password(password: str) -> bcrypt.hashpw:
+    """Return a salted hashed password
+    """
+    salt = bcrypt.gensalt()
+    hashed_pwd = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hashed_pwd
+
+
+password = "MyAmazingPassw0rd"
+print(hash_password(password))
+print(hash_password(password))
