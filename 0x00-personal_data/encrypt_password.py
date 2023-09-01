8ef3f96ff19993373403d35eb39b511c4464b6da
betiniakarandut <betiniakarandut@gmail.com>
2023-09-01 03:59:26 +0100

    Validate password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -12,3 +12,10 @@ def hash_password(password: str) -> bcrypt.hashpw:
     salted = bcrypt.gensalt()
     hashed_password = bcrypt.hashpw(password.encode("utf-8"), salted)
     return hashed_password
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Returns a boolean"""
+    if not bcrypt.checkpw(password.encode("utf-8"), hash_password(password)):
+        return False
+    return True
