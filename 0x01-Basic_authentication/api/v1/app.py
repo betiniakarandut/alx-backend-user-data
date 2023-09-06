855700603de0c65f6d67938c841f196fd52b94f8
betiniakarandut <betiniakarandut@gmail.com>
2023-09-06 03:05:18 +0000

    Basic authentication

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -2,6 +2,8 @@
 """
 Route module for the API
 """
+from api.v1.auth.auth import Auth
+from api.v1.auth.basic_auth import BasicAuth
 from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
@@ -12,6 +14,32 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+
+auth = None
+auth_type = getenv("AUTH_TYPE", auth)
+
+if auth_type == 'auth':
+    auth = Auth()
+
+if auth_type == 'basic_auth':
+    auth = BasicAuth()
+
+@before_request
+def add_method():
+    """Filtering each request"""
+    if auth is None:
+        return
+    path = ['/api/v1/status/',
+            '/api/v1/unauthorized/',
+            '/api/v1/forbidden/']
+    if request.path in path:
+        return
+
+    if not auth.authorization_header(request):
+        abort(401)
+
+    if not auth.current_user(request):
+        abort(403)
 
 
 @app.errorhandler(404)
@@ -24,7 +52,7 @@ def not_found(error) -> str:
     """
     return jsonify({"error": "Unauthorized"}), 401
 
-
+@app.errorhandler(403)
 def forbidden(error):
     """Forbidden error"""
     return jsonify({"error": "Forbidden"})
---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""Authentication module"""
+from flask import request
+from api.v1.views import app_views
+
+
+class Auth:
+    """Authentication class"""
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Request authentication"""
+        if path is None or excluded_paths is None or excluded_paths == []:
+            return True
+        if path[-1] != '/':
+            path += '/'
+        if path is in excluded_paths:
+            return False
+
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """Header authorization"""
+        if request is None:
+            return None
+        if not request.headers.get("Authorization"):
+            return None
+        return request.headers.get("Authorization", None)
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Gets current user"""
+        return None
