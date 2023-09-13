fdebd1086f8868f482c7540cc701ae618d735040
betiniakarandut <betiniakarandut@gmail.com>
2023-09-13 12:53:58 +0000

    Create:'users' table

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""Module to create a SQLAlchemy model named User"""
+import sqlalchemy
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import (create_engine,
+                        Column,
+                        Sequence,
+                        String,
+                        Integer)
+
+engine = create_engine('sqlite:///:memory:', echo=True)
+Base = sqlalchemy.orm.declarative_base()
+Column(Integer, Sequence('users_id_seq'), primary_key=True)
+
+
+class User(Base):
+    """Create users table"""
+    __tablename__ = 'users'
+
+    id = Column(Integer, Sequence('users_id_seq'), primary_key=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250))
+    reset_token = Column(String(250))
+
+    def __repr__():
+        """Prints official representation of users class"""
+        return"<User(email='%s', hashed_password='%s', session_id='%s', reset_token='%s')>" % (
+                     self.email,
+                     self.hashed_password,
+                     self.session_id,
+                     self.reset_token)
+
+
+Base.metadata.create_all(engine)
