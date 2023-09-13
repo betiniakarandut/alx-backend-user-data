e81746221a96616b4c4f3ead2afb8b6a4511e337
betiniakarandut <betiniakarandut@gmail.com>
2023-09-13 13:11:48 +0000

    Remove: 'Base.metadata.create_engine()'

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -25,11 +25,11 @@ class User(Base):
 
     def __repr__():
         """Prints official representation of users class"""
-        return"<User(email='%s', hashed_password='%s', session_id='%s', reset_token='%s')>" % (
-                     self.email,
-                     self.hashed_password,
-                     self.session_id,
-                     self.reset_token)
-
-
-Base.metadata.create_all(engine)
+        return"<User(email='%s'," +\
+              "hashed_password='%s'," +\
+              "session_id='%s'," +\
+              "reset_token='%s')>" % (
+                self.email,
+                self.hashed_password,
+                self.session_id,
+                self.reset_token)
