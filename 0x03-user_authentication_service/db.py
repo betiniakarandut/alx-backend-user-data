54c713480ce0e8f0e6372a42ff51d2a2b838b2b0
betiniakarandut <betiniakarandut@gmail.com>
2023-09-14 12:13:44 +0000

    Check:Type annotation

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -33,7 +33,7 @@ class DB:
 
     def add_user(self, email: str, hashed_password: str) -> User:
         """Add a user object"""
-        user = User(email=email, hashed_password=hashed_password)
+        user = User(email: str=email, hashed_password: str=hashed_password)
         self._session.add(user)
         self._session.commit()
         return user
