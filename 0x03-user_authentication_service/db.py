1ee838926a60379b12f6ed93bb2bf44e4e01a933
betiniakarandut <betiniakarandut@gmail.com>
2023-09-15 05:14:25 +0100

    Files are ready

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,6 +5,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 
 from user import Base, User
 
@@ -37,3 +39,26 @@ class DB:
         self._session.add(user)
         self._session.commit()
         return user
+
+    def find_user_by(self, **kwargs) -> User:
+        """Finds a user"""
+        for key, value in kwargs.items():
+            """Finds a user"""
+            if not hasattr(User, key):
+                raise InvalidRequestError
+            if User is None:
+                raise NoResultFound
+            row = self._session.query(User).filter_by(**kwargs)
+            first_row = row.first()
+            return first_row
+    
+    def update_user(self, user_id: int, **kwargs) -> None:
+        '''update and set user attributes
+        '''
+        user = self.find_user_by(id=user_id)
+        for key, value in kwargs.items():
+            if not hasattr(user, key):
+                raise ValueError
+            setattr(user, key, value)
+        self._session.commit()
+        return None
